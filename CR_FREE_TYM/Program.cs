a0fdc7954746c01c56e047b49c957f59aa09726b
Kitisak <kitisak@techconsbiz.com>
2025-01-06 16:40:45 +0700

    .

--- CR_FREE_TYM/Program.cs
+++ CR_FREE_TYM/Program.cs
@@ -5,10 +5,6 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using static CR_FREE_TYM.AdvanceFormExt;
 
 namespace CR_FREE_TYM
 {
